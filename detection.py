8b17a33eb609aa0e69b2bc7013a387d605a4e3e0
d3putation <alex.sinitca@icloud.com>
2024-04-13 16:29:04 +0500

    first commit sec

--- detection.py
+++ detection.py
@@ -3,8 +3,14 @@ import pandas as pd
 def detect_price(image):
     pass
 
-def get_analog_price(image_category):
-    file = pd.read_excel("C:\\Users\\alexs\\PycharmProjects\\flaskProject\\prices.xlsx", engine='openpyxl')
+def detect_category(image):
+    pass
 
 
+def get_analog_price():
+    file = pd.read_excel("C:\\Users\\alexs\\PycharmProjects\\flaskProject\\price.xlsx", engine='openpyxl', nrows=37)
+    for rows,columns in file.iterrows():
+        if columns.values[1] == detect_category(image):
+            print(columns.values[3])
 
+get_analog_price()
